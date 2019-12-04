f9950ed2e7154f6d575cb67f0f8708806e3491dd
Bingling Dong <bingling.dong@stonybrook.edu>
2019-12-03 19:17:16 -0500

    first commit

---
+++ src/sbdiet/SBDiet.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sbdiet;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ui.SBDietUI;
+
+/**
+ *
+ * @author hanli
+ */
+public class SBDiet extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        SBDietUI root = new SBDietUI(primaryStage);
+        root.startUI();
+        Scene scene = new Scene(root.getMainPane(), root.getpaneWidth(), root.getpaneHeight());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("SB's Diet");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
